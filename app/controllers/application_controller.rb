0ab108c59c154520337f2db085f94dc313226603
Gustavo Vicençotti <gustavovicencotti@gmail.com>
2025-01-08 22:13:15 -0300

    Fixing Security After Github Evaluation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
-  allow_browser versions: :modern
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :role ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :role ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :password, :password_confirmation ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :email, :password, :password_confirmation ])
+
+    if current_user&.admin?
+      devise_parameter_sanitizer.permit(:sign_up, keys: [ :role ])
+      devise_parameter_sanitizer.permit(:account_update, keys: [ :role ])
+    end
   end
 end
