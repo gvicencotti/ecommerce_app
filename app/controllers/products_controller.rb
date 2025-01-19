cb353d7e068f8f594212126e6dcdf5bfd0c6e87c
Gustavo Vicençotti <gustavovicencotti@gmail.com>
2025-01-19 15:43:19 -0300

    Tests fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,22 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+
+    if params[:category_id].present?
+      @products = @products.where(category_id: params[:category_id])
+    end
+
+    if params[:min_price].present?
+      @products = @products.where("price >= ?", params[:min_price])
+    end
+
+    if params[:max_price].present?
+      @products = @products.where("price <= ?", params[:max_price])
+    end
+
+    if params[:in_stock] == "true"
+      @products = @products.where("stock > 0")
+    end
   end
 
   def show
